2c49666cb58342eb8e8b8d7ea8d57db23424d97e
Runchveenti <nguyenngocanhthufpt77@gmail.com>
2024-03-26 15:12:37 +0700

    update

---
+++ nguyenngocanhthu_QLNhanVien/src/test/java/nguyenngocanhthu/QLNhanVien/NguyenngocanhthuQlNhanVienApplicationTests.java
@@ -0,0 +1,13 @@
+package nguyenngocanhthu.QLNhanVien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenngocanhthuQlNhanVienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
