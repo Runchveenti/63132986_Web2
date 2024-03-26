240f5a832cf8448c05225aed08e52bd6a917050e
Runchveenti <nguyenngocanhthufpt77@gmail.com>
2024-03-26 15:38:45 +0700

    update

---
+++ nguyenngocanhthu_QLNhanVien/nguyenngocanhthu_QLNhanVien/src/main/java/thiGK/ntu63132986/QLNhanVien/NguyenngocanhthuQlNhanVienApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu63132986.QLNhanVien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenngocanhthuQlNhanVienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenngocanhthuQlNhanVienApplication.class, args);
+	}
+
+}
