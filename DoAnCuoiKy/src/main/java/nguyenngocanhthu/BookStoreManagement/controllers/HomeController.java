9354eb81a97a3adf044d1e542b0d4e6196e36fe5
Runchveenti <nguyenngocanhthufpt77@gmail.com>
2024-04-16 21:03:52 +0700

    update HomePage

--- DoAnCuoiKy/src/main/java/nguyenngocanhthu/BookStoreManagement/controllers/HomeController.java
+++ DoAnCuoiKy/src/main/java/nguyenngocanhthu/BookStoreManagement/controllers/HomeController.java
@@ -1,5 +1,13 @@
 package nguyenngocanhthu.BookStoreManagement.controllers;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public String Index() {
+		return "index";
+	}
 }
