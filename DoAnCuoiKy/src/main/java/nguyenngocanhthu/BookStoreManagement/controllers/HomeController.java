274c25ec865295888ef0a330ed42149ac1cbe3b4
Runchveenti <nguyenngocanhthufpt77@gmail.com>
2024-04-22 18:04:19 +0700

    update code

--- DoAnCuoiKy/src/main/java/nguyenngocanhthu/BookStoreManagement/controllers/HomeController.java
+++ DoAnCuoiKy/src/main/java/nguyenngocanhthu/BookStoreManagement/controllers/HomeController.java
@@ -1,12 +1,21 @@
 package nguyenngocanhthu.BookStoreManagement.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import nguyenngocanhthu.BookStoreManagement.entity.Books;
+import nguyenngocanhthu.BookStoreManagement.services.BookServices;
 
 @Controller
 public class HomeController {
-	@GetMapping("/")
+	@Autowired
+	private BookServices service; 
+	
+	@GetMapping("/index")
 	public String Index() {
 		return "index";
 	}
@@ -19,5 +28,16 @@ public class HomeController {
 	@GetMapping("/available_books")
 	public String getAllBook() {
 		return "bookList";
+	}
+	
+	@GetMapping("/my_books")
+	public String getMyBooks() {
+		return "myBooks";
+	}
+	
+	@PostMapping("/save")
+	public String addBook(@ModelAttribute Books b) {
+		service.save(b);
+		return "redirect:/available_books";
 	}
 }
--- DoAnCuoiKy/src/main/java/nguyenngocanhthu/BookStoreManagement/repositories/BookRepository.java
+++ DoAnCuoiKy/src/main/java/nguyenngocanhthu/BookStoreManagement/repositories/BookRepository.java
@@ -1,5 +1,11 @@
 package nguyenngocanhthu.BookStoreManagement.repositories;
 
-public interface BookRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import nguyenngocanhthu.BookStoreManagement.entity.Books;
+
+@Repository
+public interface BookRepository extends JpaRepository<Books, Integer> {
+	
 }
